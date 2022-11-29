61d44ed763c8230b0f8c3cde27a17d091c61e7c9
jauumgod <69704112+jauumgod@users.noreply.github.com>
2022-11-29 09:27:34 -0300

    att app

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int numero =0, choice=0, tentativas=0;
+        String name = "";
+
+        Scanner scan = new Scanner(System.in);
+        Random randint = new Random();
+        numero = randint.nextInt(1,50);
+
+        System.out.println("BEM VINDO");
+
+        System.out.print("Digite seu nome:  ");
+        name = scan.next();
+
+        while(choice!=numero){
+            tentativas++;
+
+            System.out.print("Digite um numero entre [1-50]: ");
+            choice = scan.nextInt();
+
+            if (choice>numero){
+                System.out.print("Calma ai bb, digite um número Menor! ");
+            }
+            if (choice<numero){
+                System.out.print("Opa, digite um número Maior! ");
+            }
+
+        }
     }
 }
